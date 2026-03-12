43932fc2c1affe39ce88fd99efe8819597647f93
Gagan <gaganpreetsinghmohal@gmail.com>
2026-03-12 12:40:15 +0530

    Added the solution to the problem of finding the Minimum number of days taken to pick the bloomed flowers in a array like garden covering 'm' bouquets from k adjacent flowers

---
+++ Array/SEARCHING/Binary Search/BinarySearchOnAnswers/05_MinimumDaysToBoquets.js
@@ -0,0 +1,40 @@
+function canIpluck(numOfDays,m,k, day){
+    let noOfBoquets = 0
+    let flowers = 0;
+
+    for(let i=0; i < numOfDays.length; i++ ){
+        if(numOfDays[i] <= day){
+            flowers++
+
+            if(flowers >= k){
+                noOfBoquets++
+                flowers = 0;
+            }
+        }else{
+            flowers = 0
+        }
+       
+
+        if(noOfBoquets >= m){
+            return true;
+        }
+    }
+    return false
+}
+
+function MinimumDays(numOfDays,m,k){
+    let maxDays = Math.max(...numOfDays)
+    
+    for(let day=1; day<=maxDays; day++){
+        if(canIpluck(numOfDays,m,k,day)){
+          return day
+        }
+    }
+    return -1;
+}
+
+let numOfDays = [7,7,7,7,13,11,12,7]
+let m = 2;
+let k = 3;
+let result = MinimumDays(numOfDays, m, k)
+console.log(result);
